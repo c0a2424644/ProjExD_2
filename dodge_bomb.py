85875a44c684bdf9b8cb8d070d62e0168b237362
c0a2424644 <c0a2424644@edu.teu.ac.jp>
2025-04-22 14:48:30 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,19 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまた爆弾Rect
+    戻り値：タプル（横,縦）
+    画面内ならTrue、画面外はFalse
+    """
+    yoko, tate = True, True # 横、縦方向の用の変数
+    if rct.left < 0 or WIDTH < rct.right:  # 画面外なら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 画面外なら
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -53,7 +66,14 @@ def main():
         #if key_lst[pg.K_RIGHT]:
             #sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  # 画面外なら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 画面内に戻す
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate:  # 上下土地らかにはみ出ていたら
+            vy *= -1
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
